084b47a2d04255d7d7228bab9dc9d39d99bdc2d7
Ryan Rolfes <ryanmrolfes@gmail.com>
2017-08-26 18:06:51 -0700

    Adds more syntax

--- useful_methods_and_syntax.js
+++ useful_methods_and_syntax.js
@@ -1,3 +1,50 @@
 // expression - a line of code that returns a value
 //  *** EXPRESSIONS NEED SEMI-COLONS!! ***
-// statement - any line of code
+// statement - any line of code (functions and if/for/while blocks are statements)
+
+// variable assignments are expressions
+let x = 5;
+
+// function declarations are statements
+function foo() {}
+
+if (x === 5) { // `if` blocks are statements
+  console.log("hello"); // console.log's are expressions
+}
+
+
+// Looping Keywords
+//  continue
+let result = [];
+for (let i = 1; i < 10; i++) {
+  if (i % 3 === 0) {
+    continue;
+  }
+  result.push(i);
+}
+
+console.log(result); // [1, 2, 4, 5, 7, 8]
+
+// break
+let result1 = [];
+for (let i = 1; i < 10; i++) {
+  if (i % 3 === 0) {
+    break;
+  }
+  result1.push(i);
+}
+
+console.log(result1); // [1, 2]
+
+
+// Switch Statements
+// switch (expression) {
+//   case x:
+//     // code block
+//     break;
+//   case y:
+//     // code block
+//     break;
+//   default:
+//     // code block
+// }
